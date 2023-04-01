916f54996c4c2839e5fd5be2ba3f258fe900d430
Kelbikhanov <rusik9091@gmail.com>
2023-04-02 00:45:13 +0300

    add roles admin & user

--- app/providers/MainProvider.tsx
+++ app/providers/MainProvider.tsx
@@ -1,31 +1,40 @@
-import React, { FC } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-
-import Layout from '@/components/layout/Layout'
-import ReduxToast from './ReduxToast'
-import { Provider } from 'react-redux'
-import { store } from '@/store/store'
-import HeadProvider from './head-provider/HeadProvider'
+import ReduxToast from "./ReduxToast";
+import AuthProvider from "./auth-provider/AuthProvider";
+import HeadProvider from "./head-provider/HeadProvider";
+import Layout from "@/components/layout/Layout";
+import { store } from "@/store/store";
+import React, { FC } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
+import { TypeComponentAuthFields } from "@/shared/types/auth.types";
 
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false,
-		},
-	},
-})
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
-const MainProvider: FC<{children: React.ReactNode}> = ({ children }) => {
-	return (
-		<HeadProvider>
-		<Provider store={store}>
-		<QueryClientProvider client={queryClient}>
-			<ReduxToast />
-			<Layout>{children}</Layout>
-		</QueryClientProvider>
-		</Provider>
-		</HeadProvider>
-	)
+type MainProviderProps = {
+	children: React.ReactNode;
 }
 
-export default MainProvider
+type TypeAppProps = MainProviderProps & TypeComponentAuthFields
+
+const MainProvider: FC<TypeAppProps> = ({ children, Component }: TypeAppProps) => {
+  return (
+    <HeadProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <ReduxToast />
+          <AuthProvider Component={Component}>
+            <Layout>{children}</Layout>
+          </AuthProvider>
+        </QueryClientProvider>
+      </Provider>
+    </HeadProvider>
+  );
+};
+
+export default MainProvider;
--- pages/manage/index.tsx
+++ pages/manage/index.tsx
@@ -1,9 +1,12 @@
+import { NextPageAuth } from '@/shared/types/auth.types'
 import { NextPage } from 'next'
 
-const AdminPage: NextPage = () => {
+const AdminPage: NextPageAuth = () => {
   return (
     <div>AdminPage</div>
   )
 }
+
+AdminPage.isOnlyAdmin = true
 
 export default AdminPage
